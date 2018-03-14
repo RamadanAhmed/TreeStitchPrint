27519e24b8dd2eebfd11fb886a3fc079c8e54264
RamadanAhmed <ramadan.ahmedr93@gmail.com>
2018-03-15 00:15:04 +0200

    Clean Code Up

--- main.cpp
+++ main.cpp
@@ -9,18 +9,20 @@
 
 using std::vector;
 using std::queue;
+
 struct Node {
-    explicit Node(int _value):value(_value),left(nullptr),
-                              right(nullptr), NextBrother(nullptr){}
+    explicit Node(int _value) : value(_value), left(nullptr),
+                                right(nullptr), NextBrother(nullptr) {}
+
     // data member
     int value;
-    Node * left;
-    Node * right;
-    Node * NextBrother;
+    Node *left;
+    Node *right;
+    Node *NextBrother;
 
 };
 
-void stitchTree(Node * parent){
+void stitchTree(Node *parent) {
     queue<Node *> NodesQueue;
     NodesQueue.push(parent);
 
@@ -28,7 +30,7 @@ void stitchTree(Node * parent){
     NodesQueue.push(nullptr);
 
     while (!NodesQueue.empty()) {
-        Node * current = NodesQueue.front();
+        Node *current = NodesQueue.front();
         NodesQueue.pop();
         // if you didn't hit marker (end of level)
         if (current != nullptr) {
@@ -38,15 +40,15 @@ void stitchTree(Node * parent){
             current->NextBrother = NodesQueue.front();
 
             // add the left of current node to queue if exists
-            if (current->left){
+            if (current->left) {
                 NodesQueue.push(current->left);
             }
             // add the right of current node to queue if exists
-            if (current->right){
+            if (current->right) {
                 NodesQueue.push(current->right);
             }
         }
-        // if hit marker -> add marker at the end of queue
+            // if hit marker -> add marker at the end of queue
         else if (!NodesQueue.empty()) {
             NodesQueue.push(nullptr);
             // for printing tree
@@ -61,12 +63,12 @@ int main() {
     // create Tree
     vectorNode[0].left = &vectorNode[1];
     vectorNode[0].right = &vectorNode[2];
-    vectorNode[1].left = & vectorNode[3];
-    vectorNode[1].right = & vectorNode[4];
-    vectorNode[2].left = & vectorNode[5];
-    vectorNode[2].right = & vectorNode[6];
-    vectorNode[3].left = & vectorNode[7];
-    vectorNode[3].right = & vectorNode[8];
+    vectorNode[1].left = &vectorNode[3];
+    vectorNode[1].right = &vectorNode[4];
+    vectorNode[2].left = &vectorNode[5];
+    vectorNode[2].right = &vectorNode[6];
+    vectorNode[3].left = &vectorNode[7];
+    vectorNode[3].right = &vectorNode[8];
     std::cout << "Stitching Tree and Printing ....\n";
     // stitch Tree and print it
     stitchTree(&vectorNode[0]);
